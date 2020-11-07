308092d6d5488dae618575060ebcfd38781fa9c1
wiky147 <cc2039506@gmail.com>
2020-11-08 00:49:15 +0700

    make routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,47 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 // import components
-import HelloWorld from '../components/HelloWorld'
+import Dashboard from '../views/dashboard'
+import Operator from '../views/operator'
+import Student from '../views/student'
+import Transaction from '../views/transaction'
+import Report from '../views/report'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 export default new VueRouter({
-    history: true,
-    mode: "history",
-    routes: [
-        {
-            path: '/',
-            name: 'Hello World',
-            component: HelloWorld
-        }
-    ]
-});
+  history: true,
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/dashboard'
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard,
+    },
+    {
+        path: '/operator',
+        name: 'Operator',
+        component: Operator,
+    },
+    {
+        path: '/student',
+        name: 'Student',
+        component: Student,
+    },
+    {
+        path: '/transaction',
+        name: 'Transaction',
+        component: Transaction,
+    },
+    {
+        path: '/report',
+        name: 'Report',
+        component: Report,
+    },
+  ],
+})
