50077e2d849183e692c283161eada3508f0ce50f
Astrajingga <cc2039506@gmail.com>
2021-05-29 10:37:11 +0700

    finish dashborad

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,103 @@
+<template>
+    <v-app>
+        <v-card-title>
+            Dashboard
+        </v-card-title>
+        <v-row class="ma-3">
+            <v-col cols="4" v-for="(card, i) in cards" :key="i">
+                <v-card class="pt-5 pb-5 pr-6 pl-6" elevation="2" shaped>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="8">
+                            <p class="caption">{{ card.title }}</p>
+                        </v-col>
+                        <v-col class="d-flex flex-row-reverse" cols="6" md="4">
+                            <p class="caption">Total</p>
+                        </v-col>
+                    </v-row>
+                    <h1 class="d-flex flex-row-reverse">{{ card.total }}</h1>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-app>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    data() {
+        return {
+            cards: [
+                { title: 'Tahun Ajaran', total: 0 },
+                { title: 'Siswa', total: 0 },
+                { title: 'Kompetensi Keahlian', total: 0 },
+                { title: 'Kelas', total: 0 },
+                { title: 'Transaksi', total: 0 },
+                { title: 'Pemasukan', total: 0 },
+            ],
+        }
+    },
+    computed: {
+        ...mapGetters({
+            students: 'students/getStudents',
+            classes: 'classes/getClasses',
+            majors: 'majors/getMajors',
+            schoolYears: 'years/getSchoolYears',
+            transactions: 'transactions/getTransactions',
+        }),
+    },
+    async created() {
+        // get all students
+        await this.$store.dispatch('students/fetchStudents')
+
+        // get all majors
+        await this.$store.dispatch('majors/fetchMajors')
+
+        // get all classes
+        await this.$store.dispatch('classes/fetchClasses')
+
+        // get all schoolYears
+        await this.$store.dispatch('years/fetchSchoolYears')
+
+        // get all transactions
+        await this.$store.dispatch('transactions/fetchTransactions')
+    },
+    mounted() {
+        this.cards[0].total = this.schoolYears.length
+        this.cards[1].total = this.students.length
+        this.cards[2].total = this.majors.length
+        this.cards[3].total = this.classes.length
+        this.cards[4].total = this.transactions.length
+
+        let total = 0
+        this.transactions.forEach((element) => {
+            total += element.amountPay
+        })
+        this.cards[5].total = this.convertToRupiah(total)
+    },
+    methods: {
+        convertToRupiah: function(angka) {
+            var rupiah = ''
+            var angkarev = angka
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+            for (var i = 0; i < angkarev.length; i++)
+                if (i % 3 == 0) rupiah += angkarev.substr(i, 3) + '.'
+            return (
+                'Rp. ' +
+                rupiah
+                    .split('', rupiah.length - 1)
+                    .reverse()
+                    .join('')
+            )
+        },
+    },
+}
+</script>
+
+<style>
+.row {
+    flex: 0 1 auto;
+}
+</style>
